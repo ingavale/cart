5972cf288729c89c38eaa4cbed26d52598b8c761
ingavale <143815318+ingavale@users.noreply.github.com>
2023-09-06 15:52:58 +0530

    Update shopify-cart.ts

--- shopify-cart.ts
+++ shopify-cart.ts
@@ -38,10 +38,6 @@ export class ShopifyCart implements CartInterface {
     return this._settings;
   }
 
-  /**
-   * Use the GET /cart.js endpoint to get the cart as JSON.
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#get-cart-js | ShopifyAPI: GET /cart.js }
-   */
   public async getState(): Promise<CartState> {
     this.cartEvent('cart:requestStarted', '/cart.js');
     const response = await fetch(`${this._settings.url}/cart.js`);
@@ -50,10 +46,6 @@ export class ShopifyCart implements CartInterface {
     return this._state;
   }
 
-  /**
-   * Use the POST /cart/add.js endpoint to add product to cart.
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#post-cart-add-js | ShopifyAPI: POST /cart/add.js}
-   */
   public async addItem(items: CartItems): Promise<CartItemsResponse> {
     const data = Array.isArray(items) ? items : [items];
     const response = await this.post('/cart/add.js', JSON.stringify({ items: data }));
@@ -63,11 +55,6 @@ export class ShopifyCart implements CartInterface {
     return response as CartItemsResponse;
   }
 
-  /**
-   * Adds an item to your cart from a product form. The form must contain an input with name="id".
-   * If the quantity specified is more than what is available, the promise will be rejected and the cart state will remain unchanged
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#post-cart-add-js | ShopifyAPI: POST /cart/add.js}
-   */
   public async addItemFromForm(productForm: HTMLFormElement): Promise<CartLineItem> {
     const formData = new FormData(productForm);
     if (!formData.get('id')) {
@@ -81,11 +68,6 @@ export class ShopifyCart implements CartInterface {
     return response as CartLineItem;
   }
 
-  /**
-   * Clear all cart attributes from Shopify and return the state
-   * @see {@link https://shopify.dev/api/ajax/reference/cart#post-cart-update-js | ShopifyAPI: POST /cart/update.js}
-   * @see {@link https://shopify.dev/docs/themes/liquid/reference/objects/cart#cart-attributes | ShopifyAPI: cart-attributes }
-   */
   public async clearAttributes(): Promise<CartState> {
     const state = await this.getState();
     const data = JSON.stringify({
@@ -94,51 +76,24 @@ export class ShopifyCart implements CartInterface {
     return await this.post('/cart/update.js', data);
   }
 
-  /**
-   * Use the POST /cart/clear.js endpoint to set all quantities of all line items in the cart to zero.
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#post-cart-clear-js | ShopifyAPI: POST /cart/clear.js}
-   */
   public async clearItems(): Promise<CartState> {
     return await this.post('/cart/clear.js');
   }
-
-  /**
-   * Remove the cart note
-   * @see {@link https://shopify.dev/docs/themes/liquid/reference/objects/cart#cart-note | ShopifyAPI: cart.note }
-   */
   public async clearNote(): Promise<CartState> {
     return await this.post('/cart/update.js', JSON.stringify({ note: '' }));
   }
-
-  /**
-   * Removes an item from the cart using line item key or product id. Returns a promise which fulfills with the updated cart state.
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#post-cart-change-js | ShopifyAPI: POST /cart/change.js }
-   */
   public async removeItem(item: CartItemRemove): Promise<CartState> {
     return await this.post('/cart/change.js', JSON.stringify({ quantity: 0, ...item }));
   }
 
-  /**
-   * Update cart attributes from Shopify and return the state
-   * @see {@link https://shopify.dev/docs/themes/liquid/reference/objects/cart#cart-attributes | ShopifyAPI: cart-attributes }
-   */
   public async updateAttributes(attributes: Attributes): Promise<CartState> {
     return await this.post('/cart/update.js', JSON.stringify({ attributes: { ...attributes } }));
   }
 
-  /**
-   * The /cart/change.js endpoint changes the quantity and properties object of a cart line item.
-   * Only items already in your cart can be changed, and only one line item at a time can be changed.
-   * @see {@link https://shopify.dev/docs/themes/ajax-api/reference/cart#post-cart-change-js | ShopifyAPI: POST /cart/change.js }
-   */
   public async updateItem(item: CartItemUpdate): Promise<CartState> {
     return await this.post('/cart/change.js', JSON.stringify(item));
   }
 
-  /**
-   * Update or create the cart note
-   * @see {@link https://shopify.dev/docs/themes/liquid/reference/objects/cart#cart-note | ShopifyAPI: cart.note }
-   */
   public async updateNote(note: string): Promise<CartState> {
     return await this.post('/cart/update.js', JSON.stringify({ note: note }));
   }
